a156f2bb17d8839f6ffc3545dff2474357295b05
Lev1106 <lev.blsv@yandex.ru>
2026-02-16 13:08:48 +0500

    Added carousel for images

--- Lab4/task2/online-store/src/app/components/product-card/product-card.component.ts
+++ Lab4/task2/online-store/src/app/components/product-card/product-card.component.ts
@@ -13,10 +13,29 @@ export class ProductCardComponent {
   @Input() product!: Product;
   whatsapp!: string;
   telegram!: string;
+
+  allImages: string[] = [];
+  currentImage: string = '';
   ngOnInit() {
     let kaspi_link = encodeURIComponent(this.product.link);
     let product_name = encodeURIComponent(this.product.name);
     this.whatsapp = `https://wa.me/?text=Check out this product: ${kaspi_link}`;
     this.telegram = `https://t.me/share/url?url=${kaspi_link}&text=${product_name}`;
+    this.allImages = [this.product.image, ...(this.product.images as any)];
+    this.currentImage = this.product.image;
+  }
+  selectImage(image: string) {
+    this.currentImage = image;
+  }
+
+  next() {
+    let idx = this.allImages.indexOf(this.currentImage) + 1;
+    if (idx >= this.allImages.length) idx = 0;
+    this.currentImage = this.allImages[idx];
+  }
+  prev() {
+    let idx = this.allImages.indexOf(this.currentImage) - 1;
+    if (idx < 0) idx = this.allImages.length - 1;
+    this.currentImage = this.allImages[idx];
   }
 }
